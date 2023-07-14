2f4f911b17248a5823ce0f5830c43fefa9751a6b
Chloe Mayhew <chloe.mayhew@everylifetechnologies.com>
2023-07-14 16:21:13 +0100

    Initial Commit

---
+++ MinimizableCoordinatorBug/MinimizableCoordinatorBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimizableCoordinatorBugApp.swift
+//  MinimizableCoordinatorBug
+//
+//  Created by Chloe Mayhew on 14/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MinimizableCoordinatorBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
